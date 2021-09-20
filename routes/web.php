a2c4bf24f562403cdc89a29b2884432659399eef
Annie Chen <a.janice.chen@gmail.com>
2021-09-20 11:30:13 +0200

    added route to newCharacter form

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::get('/login', function () {
 Route::get('/register', function () {
     return view('register');
 });
+
+Route::get('/add_character', function () {
+    return view('addCharacter');
+});
