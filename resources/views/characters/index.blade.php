04359e882cfb99660711a161bd065b80d8ef4843
Annie Chen <a.janice.chen@gmail.com>
2021-09-30 15:58:16 +0200

    made cards for characters

--- resources/views/characters/index.blade.php
+++ resources/views/characters/index.blade.php
@@ -1,11 +1,19 @@
 @extends('layouts.app')
 @section('content')
-        <div>
+        <div class="container">
             <h1>Characters</h1>
-            <ul>
+            <div class="row">
                 @foreach($characters as $character)
-                    <li>{{$characters}}</li>
+                <div class="col-4 card mr-3" style="width: 18rem;">
+                    <img class="card-img-top" src="{{$character->charPortrait}}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$character->charName}}</h5>
+                        <p class="card-title">Vision: {{$character->charVision}}</p>
+                        <p class="card-text">{{$character->charLore}}</p>
+                        <a href="#" class="btn btn-primary">Favorite</a>
+                    </div>
+                </div>
                 @endforeach
-            </ul>
+            </div>
         </div>
 @endsection
