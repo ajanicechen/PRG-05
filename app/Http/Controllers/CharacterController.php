3135c4134c7751eb3ab2772db90559936a7265b8
Annie Chen <a.janice.chen@gmail.com>
2021-10-06 16:33:38 +0200

    added code to view form and send data

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -15,4 +15,20 @@ class CharacterController extends Controller
         //pass this data to the character view
         return view('/characters/index', ['characters' => $characters]);
     }
+
+    public function create(){
+        return view('characters/addCharacter');
+    }
+
+    public function store(Request $request){
+        $character = new Character;
+        $character->charName = $request->input('charName');
+        $character->charVision = $request->input('charVision');
+        $character->charLore = $request->input('charLore');
+        $character->charPortrait = $request->input('charPortrait');
+        $character->save();
+//        return redirect()->with('status','Character Added Succesfully');
+        $characters = Character::all();
+        return redirect('/characters');
+    }
 }
