38289c141e6ab886d15a5f481f3bf88867fbb06f
Annie Chen <a.janice.chen@gmail.com>
2021-10-07 14:34:12 +0200

    optional welcome page for admin

---
+++ resources/views/admin/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.adminApp')
+@section('content')
+    <main class="text-center">
+        <img src="https://static.wikia.nocookie.net/gensin-impact/images/1/1b/Character_Paimon_Portrait.png">
+        <div>
+            <h1>My Genshin</h1>
+            <p>Welcome Master U w U</p>
+        </div>
+    </main>
+@endsection
