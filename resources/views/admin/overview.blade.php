17c1241082a4e8164910077df232f94bbd6af5d8
Annie Chen <a.janice.chen@gmail.com>
2021-10-14 11:11:44 +0200

    portrait is now an img

--- resources/views/admin/overview.blade.php
+++ resources/views/admin/overview.blade.php
@@ -12,20 +12,20 @@
                 <table class="table">
                     <tr>
                         <th scope="col">ID</th>
+                        <th scope="col">Portrait</th>
                         <th scope="col">Name</th>
                         <th scope="col">Vision</th>
-                        <th class="description">Lore</th>
-                        <th scope="col">Portrait</th>
+                        <th scope="col">Lore</th>
                         <th colspan="2"></th>
                         <th><a href="">Logout</a></th>
                     </tr>
                     @foreach($characters as $character)
                     <tr>
                         <td>{{ $character["id"] }}</td>
+                        <td><img src="{{ $character["charPortrait"] }}" height="150px" width="100px"></td>
                         <td>{{ $character["charName"] }}</td>
                         <td>{{ $character["charVision"] }}</td>
                         <td>{{ $character["charLore"] }}</td>
-                        <td>{{ $character["charPortrait"] }}</td>
                         <td><a href="{{ url('details/'.$character["id"]) }}">Details</a></td>
                         <td><a href="{{ url('edit-character/'.$character["id"]) }}">Edit</a></td>
                         <td><a href="{{ url('delete-character/'.$character["id"]) }}">Delete</a></td>
