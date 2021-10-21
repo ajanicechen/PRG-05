6e2c29528ecc05bae410c0610e2d74371e73c5ee
Annie Chen <a.janice.chen@gmail.com>
2021-10-22 00:03:24 +0200

    new vision selection

--- resources/views/admin/addCharacter.blade.php
+++ resources/views/admin/addCharacter.blade.php
@@ -26,12 +26,25 @@
                             <input type="text" class="form-control" id="charName" name="charName" placeholder="Character Name">
                         </div>
                     </div>
+{{--                    <div class="form-group row">--}}
+{{--                        <label for="charVision" class="col-sm-2 col-form-label">Character Vision</label>--}}
+{{--                        <div class="col-sm-3">--}}
+{{--                            <input type="text" class="form-control" id="charVision" name="charVision" placeholder="Character Vision">--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+
                     <div class="form-group row">
                         <label for="charVision" class="col-sm-2 col-form-label">Character Vision</label>
                         <div class="col-sm-3">
-                            <input type="text" class="form-control" id="charVision" name="charVision" placeholder="Character Vision">
+                            <select name="charVision" id="charVision" class="form-control input-lg dynamic" data-dependent="state">
+                                <option selected disabled>Character Vision</option>
+                                @foreach($visions as $vision)
+                                <option value="{{ $vision->id }}">{{ $vision->vision }}</option>
+                                @endforeach
+                            </select>
                         </div>
                     </div>
+
                     <div class="form-group row">
                         <label for="charLore" class="col-sm-2 col-form-label">Character Lore</label>
                         <div class="col-sm-3">
