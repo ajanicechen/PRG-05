50199771059736f945b1001ea66b9fb43d59c9f9
Annie Chen <a.janice.chen@gmail.com>
2021-10-23 14:24:53 +0200

    updated character_table column names//works (except for vision)

--- resources/views/admin/read.blade.php
+++ resources/views/admin/read.blade.php
@@ -3,20 +3,20 @@
     <div class="container">
         <div class="card">
             <div class="card-header">
-                <h1 class="float-left">{{ $character->charName }}</h1>
+                <h1 class="float-left">{{ $character->name }}</h1>
                 <a href="/overview" class="btn btn-primary float-right">Back</a>
 
             </div>
             <div class="card-body row d-flex justify-content-center">
                     <div class="w-50">
-                        <img class="card-img-top" src="{{$character->charPortrait}}" alt="Image of {{ $character->charName }}">
+                        <img class="card-img-top" src="{{$character->portrait}}" alt="Image of {{ $character->name }}">
                         <div class="card-body">
-                            <h5 class="card-title">{{$character->charName}}</h5>
+                            <h5 class="card-title">{{$character->name}}</h5>
 {{--                            @dd($character->vision)--}}
-                            @foreach($character->vision as $vision)
-                                <p class="card-title">Vision: {{$vision->vision}}</p>
-                            @endforeach
-                            <p class="card-text">{{$character->charLore}}</p>
+{{--                            @foreach($character->vision as $vision)--}}
+{{--                                <p class="card-title">Vision: {{$vision->vision}}</p>--}}
+{{--                            @endforeach--}}
+                            <p class="card-text">{{$character->lore}}</p>
                         </div>
                     </div>
             </div>
