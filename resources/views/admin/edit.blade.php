46cbbf661334ddd7864ea4b58c2e2a3ed5a044d5
Annie Chen <a.janice.chen@gmail.com>
2021-10-23 14:16:08 +0200

    updated character_table column names//works (except for vision)

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -6,7 +6,7 @@
         @endif
         <div class="card">
             <div class="card-header">
-                <h1>Edit {{ $character->charName }}</h1>
+                <h1>Edit {{ $character->name }}</h1>
             </div>
             <div class="card-body">
                 <form action="{{ url('update-character/'.$character->id) }}" method="POST">
@@ -25,7 +25,7 @@
                     <div class="form-group row">
                         <label for="charName" class="col-sm-2 col-form-label">Character Name</label>
                         <div class="col-sm-4">
-                            <input type="text" class="form-control" id="charName" name="charName" value="{{ $character->charName }}">
+                            <input type="text" class="form-control" id="charName" name="charName" value="{{ $character->name }}">
                         </div>
                     </div>
                     {{-- Character Vision --}}
@@ -33,12 +33,12 @@
                         <label for="charVision" class="col-sm-2 col-form-label">Character Vision</label>
                         <div class="col-sm-3">
                             <select name="charVision" id="charVision" class="form-control input-lg dynamic" data-dependent="state">
-                                @foreach($character->vision as $vision)
-                                    <option value="{{ $vision->id }}"> {{$vision->vision}}</option>
-                                @endforeach
-                                @foreach($visions as $vision)
-                                    <option value="{{ $vision->id }}">{{ $vision->vision }}</option>
-                                @endforeach
+{{--                                @foreach($character->vision as $vision)--}}
+{{--                                    <option value="{{ $vision->id }}"> {{$vision->vision}}</option>--}}
+{{--                                @endforeach--}}
+{{--                                @foreach($visions as $vision)--}}
+{{--                                    <option value="{{ $vision->id }}">{{ $vision->vision }}</option>--}}
+{{--                                @endforeach--}}
                             </select>
                         </div>
                     </div>
@@ -46,14 +46,14 @@
                     <div class="form-group row">
                         <label for="charLore" class="col-sm-2 col-form-label">Character Lore</label>
                         <div class="col-sm-4">
-                            <input type="text" class="form-control" id="charLore" name="charLore" value="{{ $character->charLore }}">
+                            <input type="text" class="form-control" id="charLore" name="charLore" value="{{ $character->lore }}">
                         </div>
                     </div>
                     {{-- Character Portrait --}}
                     <div class="form-group row">
                         <label for="charPortrait" class="col-sm-2 col-form-label">Character Portrait</label>
                         <div class="col-sm-4">
-                            <input type="text" class="form-control" id="charPortrait" name="charPortrait" value="{{ $character->charPortrait }}">
+                            <input type="text" class="form-control" id="charPortrait" name="charPortrait" value="{{ $character->portrait }}">
                         </div>
                     </div>
 {{--                    <div>--}}
