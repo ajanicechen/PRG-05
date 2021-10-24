c34bde6397fc6b53d7c56e7b4eb60fff20b2cfff
Annie Chen <a.janice.chen@gmail.com>
2021-10-24 23:34:41 +0200

    cleaned up routes

--- routes/web.php
+++ routes/web.php
@@ -37,23 +37,25 @@ Route::get('/register', function () {
 //    return view('addCharacter');
 //});
 
-Route::get('/characters', [CharacterController::class,'index']);
 
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 //normal user routes
+Route::get('/characters', [CharacterController::class,'index']);
 Route::get('/profile/{username}', [UserController::class,'index']);
 Route::put('/profile/{username}',[UserController::class,'profileUpdate']);
-
+Route::get('/characters/{id}', [CharacterController::class, 'characterPage']);
+Route::post('/character/favorite',[CharacterController::class, 'favorite'])->name('favorite');
+Route::post('/character/unfavorite',[CharacterController::class, 'unfavorite'])->name('unfavorite');
 
 
 //admin routes
 Route::get('/overview', [CharacterController::class,'overview']);
-Route::get('add-character',[CharacterController::class,'create']);
-Route::post('add-character',[CharacterController::class,'store']);
-Route::get('details/{id}', [CharacterController::class, 'read']);
-Route::get('edit-character/{id}', [CharacterController::class, 'edit']);
-Route::put('update-character/{id}', [CharacterController::class, 'update']);
-Route::get('delete-character/{id}', [CharacterController::class, 'destroy']);
+Route::get('/add-character',[CharacterController::class,'create']);
+Route::post('/add-character',[CharacterController::class,'store']);
+Route::get('/details/{id}', [CharacterController::class, 'read']);
+Route::get('/edit-character/{id}', [CharacterController::class, 'edit']);
+Route::put('/update-character/{id}', [CharacterController::class, 'update']);
+Route::get('/delete-character/{id}', [CharacterController::class, 'destroy']);
